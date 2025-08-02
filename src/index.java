9eca278c8463be09530c1acdba53c78b46c73ba2
abhi1028singh <161558872+abhi1028singh@users.noreply.github.com>
2025-08-02 23:06:44 +0530

    Update and rename GarageUI.java to index.java

--- src/index.java
+++ src/index.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GarageUI {
+public class index {
     private static List<Service> selectedServices = new ArrayList<>();
     private static GarageService garageService = new GarageService();
 
